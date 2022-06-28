65231529cccb917cb276aaea5c96988321347471
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-06-28 17:57:14 -0400

    updated restaurant frame

--- Project1/Restaurant.java
+++ Project1/Restaurant.java
@@ -1,7 +1,31 @@
 package Project1;
 
 public class Restaurant {
+    private String food;
+
     public static void main(String[] args) {
         
     }
+
+    public waiterseatCust() {
+        
+    }
+
+    public bringtoChef() {
+
+    }
+
+    public custEat() {
+
+    }
+
+
+    //setters and getters 
+    public String getFood() {
+        return food;
+    }
+    public void setFood(String food) {
+        this.food = food;
+    }
+
 }
