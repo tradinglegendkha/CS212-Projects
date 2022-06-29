ef106cf5b6d535e8b75c701727f15557e86b4606
Buppi Khan <khanbuppi@gmail.com>
2022-06-28 21:12:58 -0400

    updated myRunnable, now it shows the process as I wanted, need to implement the other waiters

--- Project1/MyRunnable.java
+++ Project1/MyRunnable.java
@@ -4,14 +4,33 @@ import java.lang.*;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        for(int i=0; i<10; i++) {
-            System.out.println("Thread: "+i);
-            try {
-                Thread.sleep(1000); //time in miliseconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        Waiter waiter = new Waiter();
+        Customer customer = new Customer();
+        for(int i=0; i<1; i++) {
+            waiter.seatsCust();
+            try {Thread.sleep(1900);} catch (InterruptedException e) {e.printStackTrace();}
         }
-        System.out.println("Thread is now complete");
+        for(int i=0; i<1; i++) {
+            waiter.takesOrder();
+            try {Thread.sleep(1500);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+        for(int i=0; i<1; i++) {
+            waiter.bringsOrder();
+            try {Thread.sleep(1800);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+        for(int i=0; i<1; i++) {
+            waiter.bringsFood();
+            try {Thread.sleep(2100);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+        for(int i=0; i<1; i++) {
+            customer.eats();
+            try {Thread.sleep(3300);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+        for(int i=0; i<1; i++) {
+            customer.leaves();
+            try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+
+        System.out.println("Restaurant closed");
     }
 }
