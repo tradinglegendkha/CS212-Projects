1a811e07a6c243d73e3af730ece82b7f89f4e266
Buppi Khan <khanbuppi@gmail.com>
2022-06-28 21:57:01 -0400

    deleted some unnecessary stuff

--- Project1/Restaurant.java
+++ Project1/Restaurant.java
@@ -3,35 +3,18 @@ import java.awt.*;
 import java.lang.*;
 
 public class Restaurant {
-    private String food;
-
     public static void main(String[] args) {
         MyRunnable runnable = new MyRunnable();
         Thread thread = new Thread(runnable);
 
         thread.start(); //starting thread
 
-    }
-    /*
-    public waiterseatCust() {
+
+
+
         
     }
-
-    public bringtoChef() {
-
-    }
-
-    public custEat() {
-
-    }
-     */
-
-    //setters and getters 
-    public String getFood() {
-        return food;
-    }
-    public void setFood(String food) {
-        this.food = food;
-    }
-
+    
 }
+
+
