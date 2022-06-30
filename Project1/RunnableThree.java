cda51fe84b20fa5bf2ce66f1a788dfdb9cbbff4c
Buppi Khan <khanbuppi@gmail.com>
2022-06-29 22:33:44 -0400

    debug this

--- Project1/RunnableThree.java
+++ Project1/RunnableThree.java
@@ -5,32 +5,35 @@ public class RunnableThree implements Runnable {
     public void run() {
         WaiterThree waiter = new WaiterThree();
         Customer customer = new Customer();
-        for(int i=0; i<1; i++) {
-            waiter.seatsCust();
-            try {Thread.sleep(1900);} catch (InterruptedException e) {e.printStackTrace();}
+        synchronized(this) {    
+            for(int i=0; i<1; i++) {
+                try {wait();} catch (InterruptedException e1) {e1.printStackTrace();}
+                waiter.seatsCust();
+                try {Thread.sleep(1900);} catch (InterruptedException e) {e.printStackTrace();}
+            }
         }
         for(int i=0; i<1; i++) {
             waiter.takesOrder();
-            try {Thread.sleep(1500);} catch (InterruptedException e) {e.printStackTrace();}
-        }
-        for(int i=0; i<1; i++) {
-            waiter.bringsOrder();
             try {Thread.sleep(1800);} catch (InterruptedException e) {e.printStackTrace();}
         }
         for(int i=0; i<1; i++) {
+            waiter.bringsOrder();
+            try {Thread.sleep(1700);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+        for(int i=0; i<1; i++) {
             waiter.bringsFood();
-            try {Thread.sleep(2100);} catch (InterruptedException e) {e.printStackTrace();}
+            try {Thread.sleep(2600);} catch (InterruptedException e) {e.printStackTrace();}
         }
         for(int i=0; i<1; i++) {
             customer.eats();
-            try {Thread.sleep(3300);} catch (InterruptedException e) {e.printStackTrace();}
+            try {Thread.sleep(2600);} catch (InterruptedException e) {e.printStackTrace();}
         }
-        for(int i=0; i<1; i++) {
-            customer.leaves();
-            try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+        synchronized(this) {    
+            for(int i=0; i<1; i++) {
+                customer.leaves();
+                try {Thread.sleep(3900);} catch (InterruptedException e) {e.printStackTrace();}
+                notify();
+            }
         }
-
-
-
     }
 }
